00484c225c39627501ab17c8649de3a581fea27e
Vicent Baeza <vicentbaeza7@gmail.com>
2025-08-04 14:55:37 +0200

    UnionFind: added string support & implemented more interfaces

--- src/UnionFind.php
+++ src/UnionFind.php
@@ -6,7 +6,7 @@ namespace Vicent;
  * O(α(n)) time complexity for union() and find(). 
  */
 
-class UnionFind implements \IteratorAggregate, \Countable {
+class UnionFind implements \IteratorAggregate, \Countable, \ArrayAccess, \Stringable {
     public $parent = [];
 
     /**
@@ -22,8 +22,8 @@ class UnionFind implements \IteratorAggregate, \Countable {
      */
     public static function fromParents(array|\Traversable $parents) {
         $uf = new UnionFind();
-        foreach($parents as $p)
-            $uf->add($p);
+        foreach($parents as $key => $parent)
+            $uf->add($parent, $key);
         return $uf;
     }
 
@@ -37,82 +37,130 @@ class UnionFind implements \IteratorAggregate, \Countable {
     /**
      * Adds a new element to the structure. If a parent is not specified, then the element will be in a new set.
      */
-    public function add(?int $parentIdx = null) {
-        if($parentIdx === null)
-            $parentIdx = $this->count();
-        else if($parentIdx < 0)
-            throw new \InvalidArgumentException("Parent index cannot be negative. Parent index was: {$parentIdx}");
-        
-        $this->parent[] = $parentIdx;
+    public function add(int|string|null $parent = null, int|string|null $key = null): void {
+        if(!isset($parent))
+            $parent = $this->count();
+
+        if(!isset($key))
+            $this->parent[] = $parent;
+        else if(isset($parent[$key]))
+            throw new \InvalidArgumentException("Can't add {$key} to the UnionFind, it was already present.");
+        else
+            $this->parent[$key] = $parent;
     }
 
-    /**
-     * Finds the representative (the root node) of the set that contains [idx].
-     */
-    public function find(int $idx): int {
-        if(!$this->isRepresentative($idx))
-            $this->parent[$idx] = $this->find($this->parent[$idx]);
+    /** Finds the representative (the root node) of the set that contains an element. Returns null if the element doesn't exist */
+    public function find(int|string $element): int|string|null {
+        if(!$this->exists($element))
+            return null;
+        if(!$this->isRepresentative($element))
+            $this->parent[$element] = $this->find($this->parent[$element]);
 
-        return $this->parent[$idx];
+        return $this->parent[$element];
     }
 
-    /**
-     * Unites the sets containing [i] and [j].
-     */
-    public function union(int $i, int $j) {
-        $i = $this->find($i);
-        $j = $this->find($j);
-        $this->_link($i, $j);
+    /** Find the representative (the root node) of all given elements. */
+    public function findMany(array $elements): array {
+        $array = [];
+        foreach($elements as $element)
+            $array[$element] = $this->find($element);
+        return $array;
     }
 
-    /**
-     * Checks if [idx] is the representative of the set that contains it.
-     */
-    public function isRepresentative(int $idx): bool {
-        return $this->parent[$idx] == $idx;
-    }
-
-    /**
-     * Checks if two elements are in the same set.
-     */
-    public function sameSet(int $i, int $j): bool {
-        return $this->find($i) === $this->find($j);
-    }
-
-    /**
-     * Unites the sets represented by [i] and [j]. Throws an error if [i] or [j] are not representatives. 
-     */
-    public function link(int $i, int $j) {
-        if(!$this->isRepresentative($i) || !$this->isRepresentative($j))
-            throw new \InvalidArgumentException("To call link(), [i] and [j] must be representative of their sets.");
-        
-        $this->_link($i, $j);
-    }
-
-    /**
-     * Finds the representative of all elements.
-     */
+    /** Finds the representative (the root node) of all elements. */
     public function findAll(): array {
-        $n = $this->count();
-        for($i = 0; $i < $n; $i++)
-            $this->find($i);
-
-        return $this->parent;
+        return $this->findMany($this->elements());
     }
 
-    public function count(): int {
-        return count($this->parent);
+    /** Unites the sets containing two elements. */
+    public function union(int|string $element1, int|string $element2): void {
+        $element1 = $this->find($element1);
+        $element2 = $this->find($element2);
+        $this->_link($element1, $element2);
     }
 
-    public function getIterator(): \Traversable {
-        return new \ArrayObject($this->findAll()); // calls findAll() to find the representative of all elements
+    /** Unites all the sets that contain the given elements */
+    public function unionMany(array $elements) {
+        $elements = array_values($this->findMany($elements));
+        shuffle($elements);
+        $n = count($elements);
+        for($i = 1; $i < $n; $i++)
+            $this->_link($elements[$i], $elements[$i-1]);
     }
 
-    private function _link(int $i, int $j) {
+    /** Checks if an element exists in this UnionFind. */
+    public function exists(int|string $element): bool {
+        return isset($this->parent[$element]);
+    }
+
+    /** Checks if an element is the representative of its set */
+    public function isRepresentative(int|string $element): bool {
+        return $this->parent[$element] === $element;
+    }
+
+    /** Checks if and element is in the set represented by $setRepresentative. */
+    public function inSet(int|string $element, int|string $setRepresentative): bool {
+        return $this->find($element) === $setRepresentative;
+    }
+
+    /** Checks if two elements are in the same set. */
+    public function sameSet(int|string $element1, int|string $element2): bool {
+        return $this->find($element1) === $this->find($element2);
+    }
+
+    /** Unites the sets represented by two elements. Throws an error if either element is not a representative of their set. */
+    public function link(int|string $element1, int|string $element2): void {
+        if(!$this->isRepresentative($element1) || !$this->isRepresentative($element2))
+            throw new \InvalidArgumentException("To call link(), both elements must be the representative of their respective set.");
+        
+        $this->_link($element1, $element2);
+    }
+
+    /** Returns all the elements contained in this UnionFind. */
+    public function elements(): array {
+        return array_keys($this->parent);
+    }
+
+    private function _link(int|string $i, int|string $j): void {
         // Randomly shuffle union insertion (reduces complexity, see https://codeforces.com/blog/entry/21476)
         if(rand(0, 1) == 0) 
             $this->parent[$i] = $j;
         else
             $this->parent[$j] = $i;
+    }
+
+    // Countable interface:
+    public function count(): int {
+        return count($this->parent);
+    }
+
+    // IteratorAggregate interface:
+    public function getIterator(): \Traversable {
+        return new \ArrayObject($this->findAll()); // calls findAll() to find the representative of all elements
+    }
+    // Stringable interface:
+    public function __toString(): string {
+        $this->findAll();
+        return print_r($this->parent, true);
+    }
+
+    /** Checks if an element exists. Shorthand for exists(). */
+    public function offsetExists(mixed $offset): bool {
+        return $this->exists($offset);
+    }
+
+    /** Finds the representative (the root node) of the set that contains this element. Shorthand for find(). */
+    public function offsetGet(mixed $offset): int {
+        return $this->find($offset);
+    }
+
+    /** Joins two sets containing the provided elements. Shorthand for union(). */
+    public function offsetSet(mixed $offset, mixed $value): void {
+        $this->union($offset, $value);
+    }
+
+    /** Unsupported operation, always throws an exception. Needed for ArrayAccess */
+    public function offsetUnset(mixed $offset): void {
+        throw new \InvalidArgumentException("Operation not supported: can't unset() values from an UnionFind");
     }
 }
--- test/UnionFindTest.php
+++ test/UnionFindTest.php
@@ -13,6 +13,7 @@ class UnionFindTest extends TestCase {
         $this->assertEquals([], UnionFind::fromObject(new UnionFind())->findAll());
         $this->assertEquals([0,1,2,3,4,5,6,7,8,9], new UnionFind(10)->findAll());
         $this->assertEquals([0,1,2,3,4,5], UnionFind::fromParents([0,1,2,3,4,5])->findAll());
+        $this->assertEquals([0=>0,'a'=>'a',24=>24,'bcd'=>'bcd',4=>4,'str'=>'str'], UnionFind::fromParents([0=>0,'a'=>'a',24=>24,'bcd'=>'bcd',4=>4,'str'=>'str'])->findAll());
         $this->assertEquals([0,1,2,3,4,5,6], UnionFind::fromObject(new UnionFind(7))->findAll());
         $this->assertEquals([1,1,1,1,1], UnionFind::fromParents([1,1,1,1,1])->findAll());
         $this->assertEquals([3,2,2,3,3], UnionFind::fromObject(UnionFind::fromParents([3,2,2,3,3]))->findAll());
@@ -27,7 +28,9 @@ class UnionFindTest extends TestCase {
         $uf->add(0);
         $uf->add();
         $uf->add(2);
-        $this->assertEquals([0,1,2], $uf->findAll());
+        $uf->add('str');
+        $uf->add('str', 'str');
+        $this->assertEquals([0=>0,1=>1,2=>2,3=>'str','str'=>'str'], $uf->findAll());
     }
     public function testFind() {
         $uf = UnionFind::fromParents([1,1,4,1,4,5]);
@@ -37,11 +40,19 @@ class UnionFindTest extends TestCase {
         $this->assertEquals($uf->find(3), 1);
         $this->assertEquals($uf->find(4), 4);
         $this->assertEquals($uf->find(5), 5);
+        $this->assertEquals($uf->findMany([2, 4, 5, 0]), [2 => 4, 4 => 4, 5 => 5, 0 => 1]);
+        $uf = UnionFind::fromParents(['a'=>1,'b'=>1,'c'=>'c',1=>1,4=>'c','str'=>'str']);
+        $this->assertEquals($uf->find('a'), 1);
+        $this->assertEquals($uf->find('b'), 1);
+        $this->assertEquals($uf->find('c'), 'c');
+        $this->assertEquals($uf->find(1), 1);
+        $this->assertEquals($uf->find(4), 'c');
+        $this->assertEquals($uf->find('str'), 'str');
+        $this->assertEquals($uf->findMany(['a', 'c', 'str']), ['a' => 1, 'c' => 'c', 'str' => 'str']);
     }
     public function testUnion() {
         $uf = new UnionFind(6);
-        $uf->union(1,2);
-        $uf->union(1,3);
+        $uf->unionMany([1,2,3]);
         $uf->union(0,4);
         $this->assertEquals($uf->find(1), $uf->find(2));
         $this->assertEquals($uf->find(1), $uf->find(3));
