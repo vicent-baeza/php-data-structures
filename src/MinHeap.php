46fcb14c1d00ec6b25ba23abf8df33e2bb89f891
Vicent Baeza <vicentbaeza7@gmail.com>
2025-08-10 00:18:24 +0200

    MinHeap: additional comments

--- src/MinHeap.php
+++ src/MinHeap.php
@@ -7,35 +7,37 @@ namespace Vicent;
 class MinHeap implements \Countable {
     protected $data = [];
 
+    /** Gets the index of the parent of a node from its index. */
     public static function parent(int $idx): int { return intdiv($idx-1, 2); }
+    /** Gets the index of the left child of a node from its index. */
     public static function left(int $idx): int { return 2*$idx + 1; }
+    /** Gets the index of the right child of a node from its index. */
     public static function right(int $idx): int { return 2*$idx + 2; }
 
-    /**
-     * Constructs a new heap from a list of elements.
-     */
+    /** Constructs a new heap from a list of elements. */
     public function __construct(array $elements = []) {
         foreach($elements as $element)
             $this->add($element);
     }
 
+    /** The number of elements in the heap. */ 
     public function count(): int {
         return count($this->data);
     }
 
+    /** Checks if the heap is empty. */
     public function empty(): bool {
         return empty($this->data);
     }
 
-    private function swap(int $i, int $j) {
+    /** Auxiliary function to swap two elements from $data */
+    protected function swap(int $i, int $j) {
         $temp = $this->data[$i];
         $this->data[$i] = $this->data[$j];
         $this->data[$j] = $temp;
     }
 
-    /**
-     * Adds an element to the heap.
-     */
+    /** Adds an element to the heap. */
     public function add(int|float $element): void {
         // add element at the end
         $this->data[] = $element;
@@ -48,6 +50,7 @@ class MinHeap implements \Countable {
         }
     }
 
+    /** Maintains the min-heap property when an element is deleted. */
     protected function _heapify(int $i = 0): void {
         $l = $this->left($i);
         $r = $this->right($i);
@@ -64,6 +67,7 @@ class MinHeap implements \Countable {
         }
     }
     
+    /** Gets the smallest element from the heap & removes it from the heap. Returns null if the heap is empty. */
     public function pop(): int|float|null {
         if($this->empty())
             return null;
@@ -77,6 +81,7 @@ class MinHeap implements \Countable {
         return $root;
     }
 
+    /** Gets the smallest element from the heap. Returns null if the heap is empty. */
     public function get(): int|float|null {
         if($this->empty())
             return null;
